2e6d7d49255abc07cef409a005aabd8182798b88
saulasciante <ms1004@student.uni-lj.si>
2021-03-18 14:15:45 +0100

    working bandcut filter

--- test.py
+++ test.py
@@ -1,24 +1,29 @@
 from pydub import AudioSegment
-from pydub.playback import play
-from pydub.scipy_effects import high_pass_filter
+from pydub.scipy_effects import _mk_butter_filter
 
-myAudioFile = AudioSegment.from_mp3("song.mp3")
 
-# read in audio file and get the two mono tracks
-sound_stereo = AudioSegment.from_file(myAudioFile, format="mp3")
+# custom band cut filter
+def band_cut_filter(seg, low_cutoff_freq, high_cutoff_freq, order=10):
+    filter_fn = _mk_butter_filter([low_cutoff_freq, high_cutoff_freq], type='bandstop', order=order)
+    return seg.apply_mono_filter_to_each_channel(filter_fn)
+
+
+# Read in audio file and get the two mono tracks
+sound_stereo = AudioSegment.from_wav("songs/aeroplane.wav").apply_gain(-4)
 sound_mono = sound_stereo.split_to_mono()
-sound_monoL, sound_monoR = sound_mono[0], sound_mono[1]
+sound_monoL = sound_mono[0]
+sound_monoR = sound_mono[1]
 
 # Invert phase of the Right audio file
 sound_monoR_inv = sound_monoR.invert_phase()
 
-# Merge two L and R_inv files, this cancels out the centers
-# sound_CentersOut = sound_monoL.overlay(sound_monoR_inv)
+# Filtering with high pass filter
+filtered_stereo = band_cut_filter(sound_stereo, 55, 310)
+# finished = filtered_stereo
 
-filtered_stereo = high_pass_filter(sound_stereo, cutoff_freq=330)
-finished = filtered_stereo + sound_monoL + sound_monoR_inv
+# Merge all three sound files together one on top of the other
+merged = sound_monoL.overlay(sound_monoR_inv)
+merged = merged.overlay(filtered_stereo)
 
 # Export merged audio file
-finished.export(out_f="finished", format="mp3")
-
-play(finished)
+merged.export("songs/finished.wav", format="wav")
